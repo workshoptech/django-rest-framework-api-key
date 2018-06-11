062b97791b091c415def260005b856ab4118d69e
Dan <dan@sortedfood.com>
2018-06-11 21:44:06 +0100

    Small clarity improvements & run black

--- rest_framework_api_key/admin.py
+++ rest_framework_api_key/admin.py
@@ -1,22 +1,20 @@
-from django.contrib import admin
-from django.contrib import messages
-from rest_framework_api_key.models import APIKey
+from django.contrib import admin, messages
+
 from rest_framework_api_key.helpers import generate_key
+from rest_framework_api_key.models import APIKey
 
 
+@admin.register(APIKey)
 class ApiKeyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created', 'modified')
+    list_display = ("id", "name", "created", "modified")
 
     fieldsets = (
-        ('Required Information', {'fields': ('name',)}),
-        ('Additional Information', {'fields': ('key_message',)}),
+        ("Required Information", {"fields": ("name",)}),
+        ("Additional Information", {"fields": ("key_message",)}),
     )
-    readonly_fields = ('key_message',)
+    readonly_fields = ("key_message",)
 
-    search_fields = ('id', 'name',)
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+    search_fields = ("id", "name")
 
     def key_message(self, obj):
         if obj.key:
@@ -26,7 +24,12 @@ class ApiKeyAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if not obj.key:
             obj.key = generate_key()
-            messages.add_message(request, messages.WARNING, ('The API Key for %s is %s. Please note it since you will not be able to see it again.' % (obj.name, obj.key)))
+            messages.add_message(
+                request,
+                messages.WARNING,
+                (
+                    "The API Key for %s is %s. Please note it since you will not be able to see it again."
+                    % (obj.name, obj.key)
+                ),
+            )
         obj.save()
-
-admin.site.register(APIKey, ApiKeyAdmin)
--- rest_framework_api_key/models.py
+++ rest_framework_api_key/models.py
@@ -1,19 +1,27 @@
 import uuid
+
 from django.db import models
 
 
+class APIKeyManager(models.Manager):
+
+    def is_valid(self, api_key):
+        return self.filter(key=api_key).exists()
+
+
 class APIKey(models.Model):
-
-    class Meta:
-        verbose_name_plural = "API Keys"
-        ordering = ['-created']
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     name = models.CharField(max_length=50, unique=True)
     key = models.CharField(max_length=40, unique=True)
+
+    objects = APIKeyManager()
+
+    class Meta:
+        verbose_name_plural = "API Keys"
+        ordering = ["-created"]
 
     def __str__(self):
         return self.name
